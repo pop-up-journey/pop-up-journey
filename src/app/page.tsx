4c17c7fe321eeaed2a18656ef00598d8d89c2d32
HighRol1er <hunterjoe9999@gmail.com>
2025-05-28 18:53:12 +0900

    init + heroUI

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "@heroui/react";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center h-screen gap-10">
+      <Button color="primary">Button</Button>
+      <Button color="warning">Button</Button>
+      <Button color="primary">Button</Button>
+    </div>
+  );
+}
