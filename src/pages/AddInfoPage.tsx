92a41fabda75aa95e1223479cd515522377d5399
HighRol1er <hunterjoe9999@gmail.com>
2025-06-11 14:27:46 +0900

    feat: add-info 인삿말 이미지 추가

--- src/pages/AddInfoPage.tsx
+++ src/pages/AddInfoPage.tsx
@@ -1,9 +1,11 @@
 'use client';
+import Greeting from '@/assets/add-info/greeting.png';
 import HeroSection from '@/components/common/hero-section/HeroSection';
 import { Button } from '@heroui/button';
 import { Chip } from '@heroui/chip';
 import { Input } from '@heroui/input';
 import { Select, SelectItem } from '@heroui/select';
+import Image from 'next/image';
 import { useState } from 'react';
 
 const interestsList = [
@@ -20,7 +22,7 @@ const interestsList = [
   'Other',
 ];
 
-export const animals = [
+export const roles = [
   { key: 'participant', label: '참가자' },
   { key: 'host', label: '주최자' },
 ];
@@ -35,25 +37,23 @@ export default function AddInfoPage() {
   return (
     <main className="flex min-h-screen flex-col bg-gray-100">
       <HeroSection title="회원 정보 입력" description="회원가입을 위해 정보를 입력해주세요." />
-      <div className="flex flex-1 items-start justify-center bg-white px-4 py-12">
+      <div className="flex flex-1 items-start justify-center px-4 py-12">
         <div className="flex w-full max-w-5xl gap-12">
           {/* HACK: 회원 정보 입력 페이지 타이틀 임시 이미지 넣을까..  */}
           <div className="flex w-1/2 flex-col justify-center">
-            <h2 className="mb-2 text-3xl font-bold">회원 정보 입력</h2>
-            <p className="text-base text-gray-700">회원가입을 위해 정보를 입력해주세요.</p>
+            <Image src={Greeting} alt="greeting" />
           </div>
           {/* HACK: 오른쪽 폼 */}
           <form className="w-1/2 space-y-6">
             <Input label="Name" type="text" placeholder="사용하실 이름을 입력해주세요." />
-            <Input label="Email" type="email" placeholder="이메일을 입력해주세요." />
-            <Input label="Phone Number" type="tel" placeholder="전화번호를 입력해주세요." />
-            <Input label="Job Department" type="text" placeholder="직무를 입력해주세요." />
-            <Select items={animals} label="Favorite Animal" placeholder="Select an animal">
-              {(animal) => <SelectItem>{animal.label}</SelectItem>}
+            <Input label="Email" type="email" placeholder="example@example.com" />
+            <Input label="Phone Number" type="tel" placeholder="010-1234-5678" />
+            <Select items={roles} label="Role" placeholder="Select an role">
+              {(role) => <SelectItem>{role.label}</SelectItem>}
             </Select>
 
             <div>
-              <label className="mb-1 block font-semibold">관심사</label>
+              <label className="mb-1 block">관심사</label>
               <div className="mb-1 flex flex-wrap gap-2">
                 {interestsList.map((interest) => (
                   <Chip
@@ -67,10 +67,10 @@ export default function AddInfoPage() {
                   </Chip>
                 ))}
               </div>
-              <p className="text-xs text-gray-500">관심있는 팝업을 선택해주세요.</p>
+              <p className="text-primary/80 text-xs">관심있는 팝업을 선택해주세요.</p>
             </div>
             <Button type="submit" className="w-full">
-              Submit
+              완료
             </Button>
           </form>
         </div>
