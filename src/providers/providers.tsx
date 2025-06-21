0c3026fab87cac90ec5a210abf3769a049921d0b
HighRol1er <hunterjoe9999@gmail.com>
2025-06-21 16:06:30 +0900

    chore: 기타 변경 사항 - globals.css 주석 추가 - store/index.tsx 삭제 - Provider.tsx/ 공백 삭제

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { HeroUIProvider } from '@heroui/react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-
 import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { AuthSyncProvider } from './AuthSyncProvider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
