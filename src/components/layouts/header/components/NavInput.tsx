0e9891069d3757b43855b73271d35377ad70ed17
HighRol1er <hunterjoe9999@gmail.com>
2025-07-04 00:49:27 +0900

    feat: 브라우저 width 사이즈에 따라서 버튼, 인풋 ui 사이즈 조절

--- src/components/layouts/header/components/NavInput.tsx
+++ src/components/layouts/header/components/NavInput.tsx
@@ -1,9 +1,11 @@
+import useResizeUi from '@/hooks/useResizeUi';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { Input } from '@heroui/react';
 import { useState } from 'react';
 
 export default function NavInput() {
   const [text, setText] = useState('');
+  const { isMdUp } = useResizeUi();
 
   // NOTE: 필요할 경우 검색 기능을 ContextAPI or zustand 로 구현해야 함
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,17 +13,21 @@ export default function NavInput() {
   };
 
   return (
-    <section className="relative w-full">
-      <MagnifyingGlassIcon className="absolute top-1/2 left-2 z-10 size-5 -translate-y-1/2" />
-      <Input
-        value={text}
-        onChange={handleChange}
-        classNames={{
-          inputWrapper: 'pl-8',
-        }}
-        placeholder="관심있는 팝업을 찾아보세요!"
-        type="search"
-      />
+    <section aria-label="search-input" className="flex flex-[3] items-center justify-center">
+      <div className="relative w-full">
+        <MagnifyingGlassIcon className="absolute top-1/2 left-2 z-10 size-5 -translate-y-1/2" />
+        <Input
+          value={text}
+          onChange={handleChange}
+          classNames={{
+            inputWrapper: 'pl-8',
+          }}
+          placeholder="관심있는 팝업을 찾아보세요!"
+          type="search"
+          className="w-full"
+          size={isMdUp ? 'md' : 'sm'}
+        />
+      </div>
     </section>
   );
 }
