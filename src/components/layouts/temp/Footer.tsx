f03cf2b3a9a0c6d89aaecec8f8417e0d27d83b05
HighRol1er <hunterjoe9999@gmail.com>
2025-07-04 18:51:36 +0900

    design(footer): 원본과 비교 가능하게 Temp폴더에 따로 만들었음 - 반응형 수정 sm일 때도 contact와 git repo 보여지게 - connect -> contact - 아이콘 호버 시 애니메이션 추가 - 불필요한 주석 제거

---
+++ src/components/layouts/temp/Footer.tsx
@@ -0,0 +1,69 @@
+import { EnvelopeIcon } from '@heroicons/react/24/outline';
+import { Divider } from '@heroui/react';
+import Link from 'next/link';
+
+export default function TempFooter() {
+  return (
+    <footer className="bg-[#fff3f6]/10 text-gray-400 backdrop-blur-md">
+      <Divider />
+      <div className="mx-auto flex max-w-6xl flex-col items-center justify-between px-4 pt-5 pb-5 md:pb-20 md:flex-row">
+        <div className="mb-2 text-center md:mb-0 md:text-left">
+          <h3 className="relative mb-2 overflow-hidden bg-gradient-to-r from-purple-500 via-indigo-400 to-pink-500 bg-clip-text text-lg font-semibold text-transparent drop-shadow-[0_0_6px_rgba(255,150,200,0.5)]">
+            <span>A project by Hunter Joe & Yujeong Kim</span>
+          </h3>
+          <p className="mt-2 text-sm font-extralight">© 2025 Hunter Joe & Yujeong Kim. All rights reserved.</p>
+        </div>
+
+        <div className="flex flex-col items-start space-y-8 md:flex-row md:space-y-0 md:space-x-12">
+          <div className="md:text-left lg:block">
+            <h3 className="mb-2 font-semibold">Contact.</h3>
+            <ul className="space-y-2 text-sm">
+              <li className="space-x-2 flex cursor-pointer">
+                <Link
+                  href="mailto:hunterjoe9999@gmail.com"
+                  aria-label="Email Hunter Joe"
+                  className="inline-flex items-center space-x-2"
+                >
+                  <span>Hunter Joe</span>
+                  <EnvelopeIcon className="size-4 transition-colors duration-300 hover:scale-110 hover:text-white" />
+                </Link>
+              </li>
+              <li className="flex cursor-pointer items-center space-x-2">
+                <Link
+                  href="mailto:ninakyj@gmail.com"
+                  aria-label="Email Yujeong Kim"
+                  className="inline-flex items-center space-x-2"
+                >
+                  <span>Yujeong Kim</span>
+                  <EnvelopeIcon className="size-4 transition-colors duration-300 hover:scale-110 hover:text-white" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col items-start justify-center space-x-2 text-center md:text-left lg:flex-col lg:items-start lg:space-y-2 lg:space-x-0">
+            <h3 className="font-semibold mb-2">GitHub.</h3>
+            <ul className="text-sm">
+              <li>
+                <Link
+                  href="https://github.com/pop-up-journey/pop-up-journey"
+                  target="_blank"
+                  className="inline-flex items-center space-x-2"
+                >
+                  <span>Team Repo</span>
+                  <img
+                    src="https://cdn.simpleicons.org/github/gray"
+                    alt="GitHub Icon"
+                    width="16"
+                    height="16"
+                    className="transition-colors duration-300 hover:scale-110 hover:bg-white rounded-full"
+                  />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
