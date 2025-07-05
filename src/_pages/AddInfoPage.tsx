bb7ccc2801a97dfdb17583b3fcb7cf20003aa7c7
HighRol1er <hunterjoe9999@gmail.com>
2025-07-05 19:35:35 +0900

    refactor: bg뒤에 떠다니는 효과 분리

--- src/_pages/AddInfoPage.tsx
+++ src/_pages/AddInfoPage.tsx
@@ -1,8 +1,10 @@
 import AddInfoForm from '@/features/add-info/components/AddInfoForm';
+import FloatingBundle from '@/features/add-info/components/FloatingBundle';
 
 export default function AddInfoPage() {
   return (
-    <main aria-label="add-info-page" className="bg-gradient-to-tr from-pink-400 to-blue-400 dark:bg-black">
+    <main aria-label="add-info-page" className="bg-gradient-to-tr from-pink-400 to-blue-400">
+      <FloatingBundle />
       <AddInfoForm />
     </main>
   );
