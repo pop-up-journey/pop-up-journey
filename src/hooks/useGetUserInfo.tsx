e36fe2a6b070f578bd07d3f31e80ad33e37ef64f
HighRol1er <hunterjoe9999@gmail.com>
2025-07-07 19:05:57 +0900

    comment: 주석 삭제

--- src/hooks/useGetUserInfo.tsx
+++ src/hooks/useGetUserInfo.tsx
@@ -5,8 +5,6 @@ import type { User } from '@/types/user';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 
-//NOTE: 이걸 쓰는 곳이 3곳정도 있어서 context API로 빼는게 좋지 않을까 싶음
-// Context API로 빼면 API요청을 줄일 수 있음.
 export default function useGetUserInfo() {
   const { data: session, status } = useSession();
   const [userInfo, setUserInfo] = useState<User | null>(null);
