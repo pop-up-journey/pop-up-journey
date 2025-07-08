d82d5eccd83d2a8bf8040bf507ef6e120f355e63
HighRol1er <hunterjoe9999@gmail.com>
2025-07-08 20:41:57 +0900

    design: FloatingBundle 컴포넌트에 따른 가로 스크롤 제거

--- src/_pages/AddInfoPage.tsx
+++ src/_pages/AddInfoPage.tsx
@@ -3,7 +3,7 @@ import FloatingBundle from '@/features/add-info/components/FloatingBundle';
 
 export default function AddInfoPage() {
   return (
-    <main aria-label="add-info-page" className="bg-gradient-to-tr from-pink-400 to-blue-400">
+    <main aria-label="add-info-page" className="relative overflow-hidden bg-gradient-to-tr from-pink-400 to-blue-400">
       <FloatingBundle />
       <AddInfoForm />
     </main>
