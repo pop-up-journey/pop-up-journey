396061ae63c66df1b898f3b796e05eec73f74ea1
HighRol1er <hunterjoe9999@gmail.com>
2025-07-08 20:40:26 +0900

    fix: nameValidation 수정 - phoneValidation은 적용이 안됨

--- src/features/add-info/services/nameValidation.ts
+++ src/features/add-info/services/nameValidation.ts
@@ -4,11 +4,12 @@ export const nameSchema = z
   .string()
   .min(2, { message: '2자 이상이어야 합니다.' })
   .max(10, { message: '10자 이하여야 합니다.' })
-  .refine((val) => !/\s/.test(val), { message: '공백이 들어갈 수 없습니다.' });
+  .regex(/^\S+$/, { message: '공백을 포함할 수 없습니다.' });
 
 export const validateName = (value: string) => {
-  console.log(value);
   const result = nameSchema.safeParse(value);
+
+  console.log(result);
 
   if (!result.success) {
     return {
