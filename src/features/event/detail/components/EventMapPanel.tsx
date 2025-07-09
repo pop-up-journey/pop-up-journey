df69bad7c9810a2ec9a53cb221846811d9959f08
uj-kim <ninakyj@gmail.com>
2025-07-10 02:21:45 +0900

    refactor: (event-detail)주최자 정보 OrganizerInfo 컴포넌트로 분리

--- src/features/event/detail/components/EventMapPanel.tsx
+++ src/features/event/detail/components/EventMapPanel.tsx
@@ -2,10 +2,9 @@
 
 import Button from '@/components/common/button';
 import MapContainer from '@/features/event/detail/components/MapContainer';
+import OrganizerInfo from '@/features/event/detail/components/OrganizerInfo';
 import ShareButton from '@/features/event/detail/components/ShareButton';
 import useGeocode from '@/features/event/detail/hooks/useGeocode';
-import { ChevronDoubleRightIcon } from '@heroicons/react/24/outline';
-import { Avatar } from '@heroui/react';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
@@ -25,11 +24,7 @@ export default function EventMapPanel({ address, organizer }: KakaoMapProps) {
       {/* 주최자 */}
       {/* NOTE: 우선은 host-center로 연결해놨는데 주최자가 운영하고 있는/종료/예정인 팝업을 보여주는 페이지가 더 필요하지 않을까 싶음 */}
       <div className="mt-4 flex flex-col gap-2">
-        <Link className="mb-2 flex flex-row items-center gap-2" href={`/host-center`}>
-          <Avatar src={undefined} showFallback as="button" className="h-7 w-7 cursor-pointer" color="danger" />
-          <h4 className="text-lg font-semibold text-gray-500">{organizer}</h4>
-          <ChevronDoubleRightIcon className="h-5 w-5 text-gray-500" />
-        </Link>
+        <OrganizerInfo organizer={organizer} />
         {/* 신청 버튼*/}
         <Link href={`/event/${eventId}/participate`}>
           <Button fullWidth>신청하기</Button>
---
+++ src/features/event/detail/components/OrganizerInfo.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { ChevronDoubleRightIcon } from '@heroicons/react/24/outline';
+import { Avatar } from '@heroui/react';
+import Link from 'next/link';
+
+interface OrganizerInfoProps {
+  organizer: string;
+  avatarSrc?: string;
+  hostLink?: string;
+}
+
+export default function OrganizerInfo({ organizer, avatarSrc, hostLink = '/host-center' }: OrganizerInfoProps) {
+  return (
+    <Link href={hostLink} className="mb-2 flex items-center gap-2">
+      <Avatar src={avatarSrc} showFallback as="button" className="h-7 w-7 cursor-pointer" color="danger" />
+      <h4 className="text-lg font-semibold text-gray-500">{organizer}</h4>
+      <ChevronDoubleRightIcon className="h-5 w-5 text-gray-500" />
+    </Link>
+  );
+}
