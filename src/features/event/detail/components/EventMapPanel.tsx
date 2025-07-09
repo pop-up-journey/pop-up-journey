a5fcc2680018df68a4cc72e79451c7ba3b97aef6
uj-kim <ninakyj@gmail.com>
2025-07-10 02:04:54 +0900

    refactor: 멥 렌더링 부분 MapContainer 컴포넌트로 분리

--- src/features/event/detail/components/EventMapPanel.tsx
+++ src/features/event/detail/components/EventMapPanel.tsx
@@ -1,40 +1,27 @@
 'use client';
 
 import Button from '@/components/common/button';
+import MapContainer from '@/features/event/detail/components/MapContainer';
 import ShareButton from '@/features/event/detail/components/ShareButton';
 import useGeocode from '@/features/event/detail/hooks/useGeocode';
 import { ChevronDoubleRightIcon } from '@heroicons/react/24/outline';
 import { Avatar } from '@heroui/react';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 interface KakaoMapProps {
   address?: string;
   organizer: string;
 }
-// 맵 로드
 export default function EventMapPanel({ address, organizer }: KakaoMapProps) {
   const { eventId } = useParams();
   const safeAddress = address ?? '';
   const { position } = useGeocode(safeAddress);
 
   return (
-    <section className="h-130 w-80">
+    <main className="h-130 w-80">
       {/* 지도 */}
-      <div className="mb-6 w-full max-w-4xl transform overflow-hidden rounded-2xl shadow-2xl transition-transform hover:-translate-y-2">
-        <Map
-          id="map"
-          center={position!}
-          style={{
-            width: '100%',
-            height: '400px',
-          }}
-          level={3}
-        >
-          <MapMarker position={position!} />
-        </Map>
-      </div>
+      <MapContainer center={position!} />
       {/* 주최자 */}
       {/* NOTE: 우선은 host-center로 연결해놨는데 주최자가 운영하고 있는/종료/예정인 팝업을 보여주는 페이지가 더 필요하지 않을까 싶음 */}
       <div className="mt-4 flex flex-col gap-2">
@@ -54,6 +41,6 @@ export default function EventMapPanel({ address, organizer }: KakaoMapProps) {
           <ShareButton />
         </div>
       </div>
-    </section>
+    </main>
   );
 }
