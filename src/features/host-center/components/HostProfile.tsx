e01dd6175bf90548941599a1829f2436a7b9ec87
HighRol1er <hunterjoe9999@gmail.com>
2025-07-14 19:09:04 +0900

    design: host-profile 반응형 추가

--- src/features/host-center/components/HostProfile.tsx
+++ src/features/host-center/components/HostProfile.tsx
@@ -17,22 +17,28 @@ export default function HostProfile({ userInfo }: HostProfileProps) {
   }
 
   return (
-    <section id="defaultSt" className="mx-auto mt-8 flex max-w-5xl items-center justify-between border-b pb-8">
+    <section
+      id="defaultSt"
+      className="mx-auto mt-8 flex max-w-5xl items-center justify-between border-b px-4 pb-8 sm:px-6"
+    >
       <div className="flex items-center gap-6">
         {userInfo.image && <Image src={userInfo.image} alt="profile" width={80} height={80} className="rounded-full" />}
         <div className="flex-col items-start">
           <div className="flex items-center gap-2">
-            <div className="mt-1 text-sm text-gray-500">Host: </div>
+            <span className="mt-1 text-sm text-gray-500">Host: </span>
             <span className="text-xl font-bold">{userInfo.name}</span>
           </div>
-          <div className="mt-2 flex items-center gap-2">
-            {userInfo.email && <span className="rounded px-2 py-1 font-mono text-xs">{userInfo.email}</span>}
-            {userInfo.phone && <span className="rounded px-2 py-1 font-mono text-xs">{userInfo.phone}</span>}
+          {/* <div className="mt-2 flex items-center gap-2"> */}
+          <div className="mt-4 flex flex-col gap-4 md:flex-row">
+            {userInfo.email && <span className="rounded font-mono text-xs">{userInfo.email}</span>}
+            {userInfo.phone && <span className="rounded font-mono text-xs">{userInfo.phone}</span>}
           </div>
         </div>
       </div>
-      <div className="flex gap-3">
-        <Button onClick={() => router.push('/register')}>새로운 이벤트 등록</Button>
+      <div className="flex flex-col gap-4 md:flex-row">
+        <Button className="" onClick={() => router.push('/register')}>
+          새로운 이벤트 등록
+        </Button>
         {/* // TODO: 모달 처리 필요 */}
         <Button onClick={() => router.push('/add-info')}>프로필 수정</Button>
       </div>
