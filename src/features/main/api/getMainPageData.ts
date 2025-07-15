ee05bf593a3d4afe3394bed5e4120829566b472b
uj-kim <ninakyj@gmail.com>
2025-07-16 06:53:51 +0900

    refactor: getMainPageData.ts -> try-catch 추가

--- src/features/main/api/getMainPageData.ts
+++ src/features/main/api/getMainPageData.ts
@@ -3,10 +3,15 @@ import { getSavedPopupIds } from '@/hooks/getSavedPopupIds';
 
 // 일단 스킵
 export async function getMainPageData(userId?: string) {
-  const [ongoingEvents, upcomingEvents, likedEventIds] = await Promise.all([
-    getEvents({ status: 'ongoing' }),
-    getEvents({ status: 'upcoming', page: 1, pageSize: 4 }),
-    userId ? getSavedPopupIds(userId) : Promise.resolve([]),
-  ]);
-  return { ongoingEvents, upcomingEvents, likedEventIds };
+  try {
+    const [ongoingEvents, upcomingEvents, likedEventIds] = await Promise.all([
+      getEvents({ status: 'ongoing' }),
+      getEvents({ status: 'upcoming', page: 1, pageSize: 4 }),
+      userId ? getSavedPopupIds(userId) : Promise.resolve([]),
+    ]);
+    return { ongoingEvents, upcomingEvents, likedEventIds };
+  } catch (error) {
+    console.error('Failed to get main page data', error);
+    return { ongoingEvents: [], upcomingEvents: { events: [], totalCount: 0 }, likedEventIds: [] };
+  }
 }
