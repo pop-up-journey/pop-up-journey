42bd0981ee94f098018aab565bb1a04db95a4596
HighRol1er <hunterjoe9999@gmail.com>
2025-07-16 00:59:00 +0900

    feat: session props

--- src/features/popup-register/WrapperPopupRegister.tsx
+++ src/features/popup-register/WrapperPopupRegister.tsx
@@ -1,15 +1,20 @@
 'use client';
 
 import RegisterForm from '@/features/popup-register/components/RegisterForm';
+import type { Session } from 'next-auth';
 
 // TODO: 폼 제출이 완료된 다음에 알림 + 페이지이동(호스트센터 / 상세페이지)
 
-export default function WrapperPopupRegister() {
+interface WrapperPopupRegisterProps {
+  session: Session;
+}
+
+export default function WrapperPopupRegister({ session }: WrapperPopupRegisterProps) {
+  const hostId = session.user!.id!;
   return (
     <>
-      {/* <Script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js" strategy="lazyOnload" /> */}
       <main className="container mx-auto min-h-screen items-center justify-center p-8">
-        <RegisterForm />
+        <RegisterForm hostId={hostId} />
       </main>
     </>
   );
