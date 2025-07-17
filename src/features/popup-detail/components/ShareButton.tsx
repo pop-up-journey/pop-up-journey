02348d5d3a63b0bf638e3729f7fdb8dbf37ac8a7
uj-kim <ninakyj@gmail.com>
2025-07-18 04:48:42 +0900

    refactor: 관심 팝업 등록/해제, Form에 토스트 알림 적용

--- src/features/popup-detail/components/ShareButton.tsx
+++ src/features/popup-detail/components/ShareButton.tsx
@@ -1,17 +1,32 @@
 'use client';
 import Button from '@/components/common/button';
 import { LinkIcon } from '@heroicons/react/24/outline';
+import { addToast } from '@heroui/react';
+import throttle from 'lodash.throttle';
 import { useParams } from 'next/navigation';
+import { useRef } from 'react';
 import { useClipboard } from 'react-haiku';
 export default function ShareButton() {
   const params = useParams();
   const clipboard = useClipboard({ timeout: 2000 });
   const currentURL = `${process.env.NEXT_PUBLIC_API_URL_DEV}/popup/${params.popupId}`;
+
+  // NOTE: useRef + throttle :  2초간 재호출을 무시
+  const throttledCopy = useRef(
+    throttle(() => {
+      clipboard.copy(currentURL);
+      addToast({ title: '링크 복사 완료!', color: 'success' });
+    }, 2000)
+  );
+
+  const handleCopy = () => {
+    throttledCopy.current();
+  };
   return (
     <>
       {/* 1024px 미만: 아이콘만 */}
       <Button
-        onPress={() => clipboard.copy(currentURL)}
+        onPress={handleCopy}
         isIconOnly
         className="flex items-center justify-center rounded-lg border border-white/30 bg-white/30 shadow-2xl backdrop-blur-2xl transition hover:scale-105 lg:hidden dark:text-white"
       >
@@ -21,7 +36,7 @@ export default function ShareButton() {
       {/* 1024px 이상: 기존 버튼 */}
       <Button
         fullWidth
-        onPress={() => clipboard.copy(currentURL)}
+        onPress={handleCopy}
         className="hidden items-center justify-center rounded-lg border border-white/30 bg-white/30 font-semibold text-black shadow-2xl backdrop-blur-2xl transition hover:scale-105 lg:flex dark:text-white"
       >
         {clipboard.copied ? '링크 복사 완료!' : '공유하기'}
