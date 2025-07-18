26f9af1021c7ddfeb31a0db45a2ce2ab6cdfae97
HighRol1er <hunterjoe9999@gmail.com>
2025-07-18 14:37:45 +0900

    fix: hero-bg 에러 해결

--- src/features/main/components/MainBanner.tsx
+++ src/features/main/components/MainBanner.tsx
@@ -1,10 +1,7 @@
 export default function MainBanner() {
   return (
     // HACK : GSAP 애니메이션 적용 예정
-    <section
-      className="relative mb-10 flex h-[60vh] items-center justify-center bg-cover bg-center text-white"
-      style={{ backgroundImage: "url('/hero-bg.jpg')" }}
-    >
+    <section className="relative mb-10 flex h-[60vh] items-center justify-center bg-cover bg-center text-white">
       <div className="px-4 text-center">
         <h1 className="mb-4 text-4xl font-bold">지금 떠오르는 팝업, 한눈에 보기</h1>
         <p className="mb-6 text-lg">가장 인기 있는 팝업을 간편하게 찾아보세요</p>
