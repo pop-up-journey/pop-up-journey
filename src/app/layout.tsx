684a58f200221c41b3f55ba48d3f6de53ad26f63
uj-kim <ninakyj@gmail.com>
2025-07-21 18:59:21 +0900

    feat: 메타데이터 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,29 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: '팝업의 여정 ',
   description: '팝업의 여정',
+  metadataBase: new URL(process.env.NEXT_PUBLIC_API_URL_DEV ?? 'http://localhost:3000'),
+  openGraph: {
+    title: '팝업의 여정',
+    description: '팝업의 여정',
+    url: process.env.NEXT_PUBLIC_API_URL_DEV,
+    siteName: '팝업의 여정',
+    images: [
+      {
+        url: '/logo.png',
+        width: 1200,
+        height: 630,
+        alt: '팝업의 여정',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '팝업의 여정',
+    description: '팝업의 여정',
+    images: ['/logo.png'],
+  },
 };
 
 export default function RootLayout({
