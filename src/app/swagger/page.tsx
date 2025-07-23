3246b69d9f6ea77b34d1e120d35a3a5204173abe
HighRol1er <hunterjoe9999@gmail.com>
2025-07-23 21:56:18 +0900

    fix: 배포시 swagger 페이지 제외 코드 추가

--- src/app/swagger/page.tsx
+++ src/app/swagger/page.tsx
@@ -2,6 +2,10 @@ import ReactSwagger from '@/components/react-swagger';
 import { getApiDocs } from '@/libs/swagger';
 
 export default async function IndexPage() {
+  if (process.env.NODE_ENV === 'production') {
+    return <div>Not Found</div>;
+  }
+
   const spec = await getApiDocs();
   return (
     <section className="container mx-auto">
